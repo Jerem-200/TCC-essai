38dbccf9d7f106af09b796444a80f391c336ea8a
Jerem-200 <JEREMY.COMACLE@ICLOUD.COM>
2025-12-06 22:39:30 +0000

    maj

--- pages/09_Exposition.py
+++ pages/09_Exposition.py
@@ -296,7 +296,7 @@ with tab4:
             st.subheader("3. Ré-évaluation (Futur)")
             c3, c4 = st.columns(2)
             with c3:
-                post_attente = st.slider("Si je recommençais, probabilité catastrophe ? (0-100%)", 0, 100, 40, step=5, key="c_attente_post")
+                post_attente = st.slider("Si je recommençais maintenant, quelle est la probabilité que la catastrophe arrive ? (0-100)", 0, 100, 40, step=5, key="c_attente_post")
             with c4:
                 post_anxiete = st.slider("Si je recommençais, niveau d'anxiété ? (0-100)", 0, 100, 40, step=5, key="c_anxiete_post")
             
