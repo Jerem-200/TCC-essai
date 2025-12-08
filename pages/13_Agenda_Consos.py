c252843861416d487c829cf6d1608ea5ee969aa2
Jerem-200 <JEREMY.COMACLE@ICLOUD.COM>
2025-12-08 14:01:47 +0000

    maj

--- pages/13_Agenda_Consos.py
+++ pages/13_Agenda_Consos.py
@@ -67,7 +67,8 @@ with tab1:
     with st.form("form_addiction"):
         c_date, c_heure = st.columns(2)
         with c_date: date_evt = st.date_input("Date", datetime.now())
-        with c_heure: heure_evt = st.time_input("Heure", value=st.session_state.last_heure)
+        with c_heure: heure_evt = st.time_input("Heure", datetime.now().time())
+    
             
         st.divider()
         
